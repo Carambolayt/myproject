2cb50258264923b020143127a7207e693a1a3f71
tom_glb <goodMorning_glb@sina.com>
2020-10-12 13:48:30 +0800

    update main add master

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("创建yt分支");
+        System.out.println("master");
     }
 }
 
